818207420c9f781bb1d880a87d8a7c62423da523
Slava Baklanov <za4em.new.acc@gmail.com>
2023-06-07 11:15:59 +0300

    add icons & vue svg loader

--- apps/pomo-vue/src/App.vue
+++ apps/pomo-vue/src/App.vue
@@ -1,47 +1,15 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import AppButton from './components/AppButton.vue'
+import ReactSvg from '@pomo/assets/icons/ph_play-fill.svg'
 </script>
 
 <template>
 	<div>
-		<h1 class="bg-blue-900">123131</h1>
-		<a
-			href="https://vitejs.dev"
-			target="_blank"
-			class="text-red-400"
+		<AppButton
+			size="md"
+			variant="primary"
 		>
-			<img
-				src="/vite.svg"
-				class="logo"
-				alt="Vite logo"
-			/>
-		</a>
-		<a
-			href="https://vuejs.org/"
-			target="_blank"
-			class=""
-		>
-			<img
-				src="./assets/vue.svg"
-				class="logo vue"
-				alt="Vue logo"
-			/>
-		</a>
+			<ReactSvg />
+		</AppButton>
 	</div>
-	<HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
-	transition: filter 300ms;
-}
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ apps/pomo-vue/src/components/AppButton.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+	size: 'sm' | 'md' | 'lg'
+	variant: 'primary' | 'secondary' | 'transparent'
+}>()
+</script>
+
+<template>
+	<button
+		type="button"
+		class="text-primary-900 dark:text-primary-50 dark:[&>svg_path]:stroke-primary-50 dark:[&>svg_path]:fill-primary-50 transition-colors"
+		:class="{
+			'rounded-sm p-2': size === 'sm',
+			'rounded-md p-4 xs:p-6': size === 'md',
+			'rounded-lg px-10 py-6 xs:px-12 xs:py-8': size === 'lg',
+			'bg-primary-a-600 hover:bg-primary-a-700 active:bg-primary-a-800': variant === 'primary',
+			'bg-primary-a-100 hover:bg-primary-a-200 active:bg-primary-a-300': variant === 'secondary',
+			'bg-transparent': variant === 'transparent',
+		}"
+	>
+		<slot />
+	</button>
+</template>
