969789e88c73830c5fa84c3455b2577f480be30d
Slava Baklanov <za4em.new.acc@gmail.com>
2023-06-19 11:50:40 +0300

    [angular] add chip component

--- apps/pomo-angular/src/app/app.module.ts
+++ apps/pomo-angular/src/app/app.module.ts
@@ -1,17 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { AngularSvgIconModule } from 'angular-svg-icon';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { HttpClientModule } from '@angular/common/http'
+import { AngularSvgIconModule } from 'angular-svg-icon'
 
-import { AppComponent } from './app.component';
-import { ChipComponent } from './chip/chip.component';
-import { InputNumberComponent } from './input-number/input-number.component';
-import { SwitchComponent } from './switch/switch.component';
-import { TimerComponent } from './timer/timer.component';
-import { ButtonComponent } from './button/button.component';
+import { AppComponent } from './app.component'
+import { ChipComponent } from './chip/chip.component'
+import { InputNumberComponent } from './input-number/input-number.component'
+import { SwitchComponent } from './switch/switch.component'
+import { TimerComponent } from './timer/timer.component'
+import { ButtonComponent } from './button/button.component'
 
 @NgModule({
-  declarations: [AppComponent, ChipComponent, InputNumberComponent, SwitchComponent, TimerComponent, ButtonComponent],
+  declarations: [
+    AppComponent,
+    ChipComponent,
+    InputNumberComponent,
+    SwitchComponent,
+    TimerComponent,
+    ButtonComponent,
+  ],
   imports: [BrowserModule, HttpClientModule, AngularSvgIconModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
--- apps/pomo-angular/src/app/chip/chip.component.ts
+++ apps/pomo-angular/src/app/chip/chip.component.ts
@@ -1,26 +1,25 @@
-import { Component, Input } from '@angular/core';
-// import BrainSvg from '@pomo/assets/icons/ph_brain-fill.svg';
-// import CoffeeSvg from '@pomo/assets/icons/ph_coffee.svg';
+import { Component, Input } from '@angular/core'
 
-type TimerState = 'focus' | 'break-short' | 'break-long';
+type TimerState = 'focus' | 'break-short' | 'break-long'
 @Component({
   selector: 'app-chip',
   templateUrl: './chip.component.html',
 })
 export class ChipComponent {
-  @Input({ required: true }) variant: TimerState = 'focus';
+  @Input({ required: true }) variant: TimerState = 'focus'
+
   config = {
     focus: {
-      icon: '@pomo/assets/icons/ph_brain-fill.svg',
+      icon: '/assets/icons/ph_brain-fill.svg',
       text: 'Focus',
     },
     'break-long': {
-      icon: '@pomo/assets/icons/ph_coffee.svg',
+      icon: '/assets/icons/ph_coffee.svg',
       text: 'Break long',
     },
     'break-short': {
-      icon: '@pomo/assets/icons/ph_coffee.svg',
+      icon: '/assets/icons/ph_coffee.svg',
       text: 'Break short',
     },
-  };
+  }
 }
